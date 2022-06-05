7d985ffb15468583ebb2dc1e299cfe08291ebad4
amira <amiraabouhadid@gmail.com>
2022-06-05 13:17:07 +0200

    change api type from any to defined type

--- src/portal/lib/farms.ts
+++ src/portal/lib/farms.ts
@@ -4,8 +4,9 @@ import axios from 'axios';
 import config from '../config';
 import { getNodeUsedResources } from './nodes';
 import { hex2a } from './util'
-export async function getFarm (api: any, twinID:number) {
+export async function getFarm (api: { query: any; }, twinID:number) {
     const farms = await api.query.tfgridModule.farms.entries()
+ 
     const twinFarms = farms.filter((farm: { toJSON: () => { (): any; new(): any; twin_id: number } }[]) => {
       if (farm[1].toJSON().twin_id === twinID) {
         return farm
