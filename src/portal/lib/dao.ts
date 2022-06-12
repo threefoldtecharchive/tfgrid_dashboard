7a7007b19b05d1272887fcdfb3cedb6dde32075f
amira <amiraabouhadid@gmail.com>
2022-06-12 21:23:00 +0200

    add vote modal

--- src/portal/lib/dao.ts
+++ src/portal/lib/dao.ts
@@ -1,7 +1,17 @@
 
 import { hex2a } from './util'
 import moment from 'moment'
-
+import { web3FromAddress } from '@polkadot/extension-dapp';
+export async function vote(address: string, api: { tx: { dao: { vote: (arg0: any, arg1: any, arg2: any) => { (): any; new(): any; signAndSend: { (arg0: any, arg1: { signer: any }, arg2: any): any; new(): any } } } } }, farmId: string, hash: any, approve: any, callback: any) {
+    try {
+        const injector = await web3FromAddress(address)
+        return api.tx.dao
+            .vote(farmId, hash, approve)
+            .signAndSend(address, { signer: injector.signer }, callback)
+    } catch (error) {
+        console.log(`err while trying to get injector ${error}`)
+    }
+}
 export async function getProposals(api: any) {
     const proposals: any = [];
     const hashes = await api.query.dao.proposalList()
@@ -22,7 +32,6 @@ export async function getProposals(api: any) {
             link: hex2a(daoProposal.link)
         })
     })
-    console.log(proposals)
     return proposals
 }
 
--- src/portal/views/Dao.vue
+++ src/portal/views/Dao.vue
@@ -37,18 +37,80 @@
         </v-card-subtitle>
         <v-card-text>
           {{proposal.description}}
-        </v-card-text>
-        <v-card-actions>
           <a
             v-bind:href="proposal.link"
             v-bind:target="'blank'"
-          >View</a>
-          <v-btn>Vote</v-btn>
+          >More details</a>
+
+        </v-card-text>
+        <v-card-actions class="d-flex justify-end">
+          <v-btn @click="openVoteDialog = true">Vote</v-btn>
 
         </v-card-actions>
+        <v-dialog
+          v-model="openVoteDialog"
+          transition="dialog-bottom-transition"
+          max-width="600"
+        >
+          <v-card>
+            <v-toolbar
+              color="primary"
+              dark
+            >
+              Vote on {{proposal.action}}
+            </v-toolbar>
+            <v-card-text>
+              <v-container
+                fluid
+                class="mt-5"
+              >
+                <v-row>
+                  ID: {{proposal.hash}}
+                </v-row>
+                <v-row>
+                  Description: {{proposal.description}}
+                </v-row>
+                <v-row>
+                  <a
+                    v-bind:href="proposal.link"
+                    v-bind:target="'blank'"
+                  >More details</a>
+                </v-row>
+              </v-container>
+
+            </v-card-text>
+            <v-container>
+              <div class="d-flex justify-center pa-5">
+                <v-btn
+                  color="green"
+                  class="mx-5"
+                  style="padding:7.5%"
+                >Yes <br>{{ proposal.ayes.length}}</v-btn>
+                <v-btn
+                  color="red"
+                  class=" mx-5"
+                  style="padding:7.5%"
+                >No <br>{{proposal.nayes.length}}</v-btn>
+              </div>
+              <div>
+                <v-progress-linear
+                  height="25"
+                  v-bind:value="getProgress(proposal.hash)"
+                >
+                  <strong>{{ getProgress(proposal.hash)  }}%</strong>
+                </v-progress-linear>
+
+              </div>
+
+            </v-container>
+
+          </v-card>
+
+        </v-dialog>
       </v-card>
 
     </v-container>
+
     <v-container v-else>
       <v-card class="my-3 pa-3 d-flex justify-center">
         <h3>No Active proposals at this time</h3>
@@ -68,11 +130,32 @@ export default class DaoView extends Vue {
   openInfoModal = false;
   proposals: any = [];
   $api: any;
-
+  openVoteDialog = false;
+  percentageVoted = 0;
   async mounted() {
     if (this.$api) {
       this.proposals = await getProposals(this.$api);
     }
+  }
+  getProgress(hash: any) {
+    const proposal = this.proposals.filter(
+      (p: { hash: any }) => p.hash === hash
+    )[0];
+
+    const totalAyeWeight = proposal.ayes.reduce(
+      (total: number, v: { weight: number }) => total + v.weight,
+      0
+    );
+    const totalNayeWeight = proposal.nayes.reduce(
+      (total: number, v: { weight: number }) => total + v.weight,
+      0
+    );
+
+    const total = totalAyeWeight + totalNayeWeight;
+    if (total > 0) {
+      return (totalAyeWeight / total) * 100;
+    }
+    return 0;
   }
 }
 </script>
