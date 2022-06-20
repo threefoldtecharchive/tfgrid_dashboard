c7c034e77f010fddd6d69a58d38db0c610752c82
MohamedElmdary <engm5081@gmail.com>
2022-06-20 16:39:27 +0200

    Updated config build script

--- public/config.js
+++ public/config.js
@@ -1,14 +1,14 @@
-window.configs = window.configs || {};
-window.configs.gql_url = "https://graphql.dev.grid.tf/graphql";
-window.configs.proxy_url = "https://gridproxy.dev.grid.tf";
-//window.configs.polkadot_url = "wss://tfchain.dev.grid.tf/ws";
-window.configs.version = "v3.0.0-rc14";
-window.configs.APP_API_URL= 'wss://tfchain.dev.grid.tf/ws';
-window.configs.APP_STELLAR_HORIZON_URL='https://horizon-testnet.stellar.org'
-window.configs.APP_TFT_ASSET_ISSUER='GA47YZA3PKFUZMPLQ3B5F2E3CJIB57TGGU7SPCQT2WAEYKN766PWIMB3'
-window.configs.APP_BRIDGE_TFT_ADDRESS='GBLQSUJUD3HTC6YVCOIYI57QX6Q37AR4TNGVCH2YCSKH7YMUUHECHKPN'
-window.configs.APP_ACTIVATION_SERVICE_URL='https://activation.dev.grid.tf'
-window.configs.APP_EXPLORER_URL='https://explorer.dev.grid.tf/'
-window.configs.APP_GRAPHQL_URL='https://graphql.dev.grid.tf/graphql/'
-window.configs.APP_GRIDPROXY_URL='https://gridproxy.dev.grid.tf/'
-window.configs.APP_NETWORK='dev'
+window.configs = {
+  APP_API_URL: "wss://tfchain.dev.grid.tf/ws",
+  APP_STELLAR_HORIZON_URL: "https://horizon-testnet.stellar.org",
+  APP_TFT_ASSET_ISSUER:
+    "GA47YZA3PKFUZMPLQ3B5F2E3CJIB57TGGU7SPCQT2WAEYKN766PWIMB3",
+  APP_BRIDGE_TFT_ADDRESS:
+    "GBLQSUJUD3HTC6YVCOIYI57QX6Q37AR4TNGVCH2YCSKH7YMUUHECHKPN",
+  APP_ACTIVATION_SERVICE_URL: "https://activation.dev.grid.tf",
+  APP_EXPLORER_URL: "https://explorer.dev.grid.tf/",
+  APP_GRAPHQL_URL: "https://graphql.dev.tf/graphql",
+  APP_GRIDPROXY_URL: "https://gridproxy.dev.grid.tf",
+  APP_NETWORK: "dev",
+  APP_VERSION: "dev1.0.0",
+};
