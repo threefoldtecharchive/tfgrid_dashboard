656e66263f303f22eb7c1aa34bf81ebcb5fec010
ahmedhanafy725 <hanafya@incubaid.com>
2022-07-07 15:07:04 +0200

    Hide delete action on farms

--- src/portal/views/Farms.vue
+++ src/portal/views/Farms.vue
@@ -283,7 +283,7 @@ export default class FarmsView extends Vue {
     { text: "Linked Twin ID", value: "twin_id" },
     { text: "Certification type", value: "certification" },
     { text: "Pricing Policy ID", value: "pricing_policy_id" },
-    { text: "Actions", value: "actions" },
+    // { text: "Actions", value: "actions" },
   ];
   farms: any = [];
   id: any = [];
