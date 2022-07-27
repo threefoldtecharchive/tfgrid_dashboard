0d3b64b8ac6c75957f66f25f92ed4a284ad2662d
amira <amiraabouhadid@gmail.com>
2022-07-27 15:51:27 +0200

    update delete IP button color

--- src/portal/components/DeleteIP.vue
+++ src/portal/components/DeleteIP.vue
@@ -6,8 +6,6 @@
     >
       <template v-slot:activator="{ on, attrs }">
         <v-btn
-          color="secondary"
-          dark
           v-bind="attrs"
           v-on="on"
           outlined
