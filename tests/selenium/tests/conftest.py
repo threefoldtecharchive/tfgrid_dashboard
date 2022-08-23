8bbe9990fb89c6b82edfb2ac4db3bc3fdb9f8171
Ahmed Saleh <ahmed.saleh.harby@gmail.com>
2022-08-23 13:48:13 +0200

    fixing selenium workflow

--- tests/selenium/tests/conftest.py
+++ tests/selenium/tests/conftest.py
@@ -13,7 +13,6 @@ def browser():
   # Initialize the ChromeDriver instance with options
   options = webdriver.ChromeOptions()
   options.add_argument('--no-sandbox')
-  options.add_argument('--disable-dev-shm-usage')
   options.add_extension('tests/selenium/polka_extension_0_44_1_0.crx')  #PolkaDot Extension
   options.add_argument("--start-maximized")
   driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
