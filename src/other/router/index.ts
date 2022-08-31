fb24da312b9c174ce6ead5a8b45eed3c0740960a
MohamedElmdary <engm5081@gmail.com>
2022-08-31 12:18:37 +0200

    Remove Hub from PR

--- src/other/router/index.ts
+++ src/other/router/index.ts
@@ -1,15 +1,10 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import HubView from "../views/HubView.vue";
 import BootstrapView from "../views/BootstrapView.vue";
 
 Vue.use(VueRouter);
 
 export const otherRoutes: Array<RouteConfig> = [
-  {
-    path: "/other/hub",
-    component: HubView,
-  },
   {
     path: "/other/bootstrap",
     component: BootstrapView,
