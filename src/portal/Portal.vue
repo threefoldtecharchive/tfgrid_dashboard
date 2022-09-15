dbb6861ce884f185e8dd20ab783fdcccb7ede522
zaelgohary <zainabelgohary19@gmail.com>
2022-09-15 18:24:01 +0200

    remove unused code

--- src/portal/Portal.vue
+++ src/portal/Portal.vue
@@ -1,18 +1,14 @@
 <template>
   <v-container>
-
-
     <router-view style="padding: 6% 5% 5% 10%; margin: 4% 0" />
   </v-container>
 
 </template>
 <script lang="ts">
-import FundsCard from "./components/FundsCard.vue";
 import { Component, Vue } from "vue-property-decorator";
 
 @Component({
   name: "PortalView",
-  components: { FundsCard },
 })
 export default class PortalView extends Vue {
 
