3647a68fb21dd98c915354031e896ab5f62dc2b7
Ahmed Saleh <ahmed.saleh.harby@gmail.com>
2022-09-28 22:08:01 +0200

    add selenium workflow actions

--- tests/frontend_selenium/tests/conftest.py
+++ tests/frontend_selenium/tests/conftest.py
@@ -2,6 +2,7 @@ import pytest
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
+from pyvirtualdisplay import Display
 
 """
 This module contains shared browser fixtures.
@@ -10,11 +11,14 @@ This module contains shared browser fixtures.
 @pytest.fixture
 def browser():
 
+    display = Display(visible=0, size=(1500, 1200))
+    display.start()
+
     # Initialize the ChromeDriver instance with options
     options = webdriver.ChromeOptions()
     options.add_extension('polka_extension_0_44_1_0.crx')  #PolkaDot Extension
-    options.add_argument("--start-maximized")
     driver = webdriver.Chrome(options=options, service=Service(ChromeDriverManager().install()))
+    driver.set_window_size(1200, 1100)
 
     # Make its calls wait up to 20 seconds for elements to appear
     driver.implicitly_wait(60)
@@ -24,3 +28,5 @@ def browser():
 
     # Quit the WebDriver instance for the cleanup
     driver.quit()
+
+    display.stop()
