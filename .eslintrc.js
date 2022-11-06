35641e010d30bf712d12b83e51d4d090e48ef9a5
A-Harby <harbya@incubaid.com>
2022-11-06 19:07:52 +0200

    force_linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,30 +1,36 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true,
+    es2021: true,
+    node: true,
   },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/typescript/recommended'
+  extends: [
+    "plugin:vue/vue3-essential",
+    "eslint:recommended",
+    "@vue/typescript/recommended",
+    "standard-with-typescript",
+    "prettier",
   ],
   parserOptions: {
-    ecmaVersion: 2020
+    sourceType: "module",
+    ecmaVersion: 2020,
   },
+  plugins: ["vue"],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/multi-word-component-names': 'off', 
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/multi-word-component-names": "off",
   },
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
       ],
       env: {
-        jest: true
-      }
-    }
-  ]
-}
+        jest: true,
+      },
+    },
+  ],
+};
