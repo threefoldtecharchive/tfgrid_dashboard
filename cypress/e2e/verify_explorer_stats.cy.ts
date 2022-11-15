1ca82a76f70c16c97bf99239c18825f2b5506861
mohamedamer453 <amerm@incubaid.com>
2022-11-15 12:34:34 +0200

    fix errors/format detected from linting

--- cypress/e2e/verify_explorer_stats.cy.ts
+++ cypress/e2e/verify_explorer_stats.cy.ts
@@ -1,20 +1,15 @@
 /// <reference types="cypress"/>
 
-import statsPage from "../page-objects/stats-page"
-import utils from "../utils/utils"
+import statsPage from "../page-objects/stats-page";
+import utils from "../utils/utils";
 
+describe("TF Grid Dashboard", function () {
+  this.beforeEach(function () {
+    statsPage.Visit();
+  });
 
-describe('TF Grid Dashboard', function(){
-
-
-    this.beforeEach(function(){
-        statsPage.Visit()
-    })
-
-
-    it('Verify Statistics', function(){
-
-        /**************************************************
+  it("Verify Statistics", function () {
+    /**************************************************
         Test Suite: TF Grid Dashboard
         Test Cases: TC823 - Verify Statistics
         Scenario:
@@ -22,31 +17,39 @@ describe('TF Grid Dashboard', function(){
             page from the grid proxy
         **************************************************/
 
-        cy.request({
-            method: 'GET',
-            url:'https://gridproxy.dev.grid.tf/stats?status=up',
+    cy.request({
+      method: "GET",
+      url: "https://gridproxy.dev.grid.tf/stats?status=up",
+    }).should((res) => {
+      //Get the stats from the grid_proxy
+      const nodesOnline = JSON.stringify(res.body.nodes);
+      const farms = JSON.stringify(res.body.farms);
+      const countries = JSON.stringify(res.body.countries);
+      const totalCru = JSON.stringify(res.body.totalCru);
+      const totalHru = utils.formatBytes(JSON.stringify(res.body.totalHru));
+      const totalSru = utils.formatBytes(JSON.stringify(res.body.totalSru));
+      const totalMru = utils.formatBytes(JSON.stringify(res.body.totalMru));
+      const accessNodes = JSON.stringify(res.body.accessNodes);
+      const gateways = JSON.stringify(res.body.gateways);
+      const twins = JSON.stringify(res.body.twins);
+      const publicIps = JSON.stringify(res.body.publicIps);
+      const contracts = JSON.stringify(res.body.contracts);
 
-        }).should((res)=>{
-            
-            //Get the stats from the grid_proxy
-            let nodesOnline = JSON.stringify(res.body.nodes)
-            let farms       = JSON.stringify(res.body.farms)
-            let countries   = JSON.stringify(res.body.countries)
-            let totalCru    = JSON.stringify(res.body.totalCru)
-            let totalHru    = utils.formatBytes(JSON.stringify(res.body.totalHru))
-            let totalSru    = utils.formatBytes(JSON.stringify(res.body.totalSru))
-            let totalMru    = utils.formatBytes(JSON.stringify(res.body.totalMru))
-            let accessNodes = JSON.stringify(res.body.accessNodes)
-            let gateways    = JSON.stringify(res.body.gateways)
-            let twins       = JSON.stringify(res.body.twins)
-            let publicIps   = JSON.stringify(res.body.publicIps)
-            let contracts   = JSON.stringify(res.body.contracts)
-
-            //Verify the values appearing in the statisticspage from the grid proxy
-            statsPage.VerifyStats(nodesOnline, farms, countries, totalCru, totalSru,
-                totalHru, totalMru, accessNodes, gateways, twins, publicIps, contracts)
-        })
-        
-    })
-
-})
+      //Verify the values appearing in the statisticspage from the grid proxy
+      statsPage.VerifyStats(
+        nodesOnline,
+        farms,
+        countries,
+        totalCru,
+        totalSru,
+        totalHru,
+        totalMru,
+        accessNodes,
+        gateways,
+        twins,
+        publicIps,
+        contracts
+      );
+    });
+  });
+});
