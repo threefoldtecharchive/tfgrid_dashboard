087ebce5116769dbad1bf42689a24ad053e49ed8
maayarosama <mayarosama18@gmail.com>
2022-11-28 17:15:18 +0200

    Fixing Reserved Balance

--- src/portal/lib/balance.ts
+++ src/portal/lib/balance.ts
@@ -1,21 +1,33 @@
-import { Keyring } from '@polkadot/keyring'
+import { Keyring } from "@polkadot/keyring";
 export interface balanceInterface {
   free: number;
   reserved: number;
 }
 
-export async function getBalance(api: { query: { system: { account: (arg0: string) => { data: { free: { toJSON: () => number; }; reserved: { toJSON: () => number; }; } } } } }, address: string) {
-  const res = await api.query.system.account(address)
+export async function getBalance(
+  api: {
+    query: {
+      system: {
+        account: (arg0: string) => { data: { free: { toJSON: () => number }; feeFrozen: { toJSON: () => number } } };
+      };
+    };
+  },
+  address: string,
+) {
+  const res = await api.query.system.account(address);
 
-  return { free: res.data.free.toJSON() / 1e7, reserved: res.data.reserved.toJSON() / 1e7 }
-
+  return { free: res.data.free.toJSON() / 1e7, reserved: res.data.feeFrozen.toJSON() / 1e7 };
 }
 
-export async function getMoreFunds(substrateAccountID: string, api: { tx: { balances: { transfer: (arg0: string, arg1: number) => any } } }, callback: any) {
-  const keyring = new Keyring({ type: 'sr25519' })
-  const alice = keyring.addFromUri('//Alice')
+export async function getMoreFunds(
+  substrateAccountID: string,
+  api: { tx: { balances: { transfer: (arg0: string, arg1: number) => any } } },
+  callback: any,
+) {
+  const keyring = new Keyring({ type: "sr25519" });
+  const alice = keyring.addFromUri("//Alice");
 
-  const transfer = await api.tx.balances.transfer(substrateAccountID, 100 * 1e7)
+  const transfer = await api.tx.balances.transfer(substrateAccountID, 100 * 1e7);
 
-  transfer.signAndSend(alice, callback)
+  transfer.signAndSend(alice, callback);
 }
