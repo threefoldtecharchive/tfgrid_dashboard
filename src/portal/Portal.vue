7b3c805d7e7c4a86707b3852f22909c61bc59704
MohamedElmdary <engm5081@gmail.com>
2023-04-10 17:27:33 +0200

    Moved votePopup into it's own component

--- src/portal/Portal.vue
+++ src/portal/Portal.vue
@@ -1,52 +1,18 @@
 <template>
   <v-container>
     <router-view style="padding: 6% 5% 5% 10%; margin: 4% 0" />
-    <v-dialog v-model="openProposalDialog" persistent max-width="350">
-      <template v-slot:default="dialog">
-        <v-card>
-          <v-toolbar color="primary" dark dense elevation="4" outlined rounded>Vote now!</v-toolbar>
-          <div class="d-flex align-center">
-            <v-card-text>
-              <div class="text-subtitle-1 text-center">{{ voteMsg }}</div>
-            </v-card-text>
-          </div>
-          <v-card-actions class="justify-space-around">
-            <v-btn text @click="dialog.value = false">Close</v-btn>
-            <v-btn text @click="daoRedirect">Vote</v-btn>
-          </v-card-actions>
-        </v-card>
-      </template>
-    </v-dialog>
+    <VotePopup v-if="$store.state.credentials.twin.id" />
   </v-container>
 </template>
 <script lang="ts">
-import { Component, Vue, Watch } from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
+import VotePopup from "./components/VotePopup.vue";
 
 @Component({
   name: "PortalView",
+  components: {
+    VotePopup,
+  },
 })
-export default class PortalView extends Vue {
-  openProposalDialog = false;
-  proposals = 0;
-  voteMsg = "You have a pending proposal to vote!";
-  @Watch("$store.state.credentials.twin.id")
-  async getProposals() {
-    if (!this.$store.state.credentials.twin.id) return;
-    let twin = this.$store.state.credentials.twin.id;
-    await this.$store.dispatch("portal/getProposal", twin);
-    this.proposals = this.$store.state.portal.proposals;
-    if (!this.proposals) return;
-    this.voteMsg =
-      this.proposals > 1
-        ? `You have ${this.proposals} pending proposals to vote!`
-        : `You have a pending proposal to vote!`;
-    if (twin === this.$store.state.credentials.twin.id) this.openProposalDialog = true;
-  }
-  daoRedirect() {
-    this.openProposalDialog = false;
-    this.$router.push({
-      path: "account-dao",
-    });
-  }
-}
+export default class PortalView extends Vue {}
 </script>
